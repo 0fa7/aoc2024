0ca8d210041e66fc25f61004e8cc9a34ab9ec6a2
0fa7 <7063015+0fa7@users.noreply.github.com>
2024-12-02 11:48:42 -0500

    project start and day 1 solutions

---
+++ 01.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
+int main()
+{
+  // part 1
+  std::vector<int> c0(1000, 0);
+  std::vector<int> c1(1000, 0);
+  std::unordered_map<int, int> r_counts;
+
+  std::string line;
+
+  std::ifstream input("../01_input.txt");
+
+  int i = 0;
+
+  while(getline(input, line))
+  {
+    std::string tok;
+    std::stringstream ss(line);
+
+    ss >> c0[i];
+    ss >> c1[i];
+    r_counts[c1[i]]++;
+
+    std::cout << c0[i] << " " << c1[i] << std::endl;
+    i++;
+  }
+
+  std::sort(c0.begin(), c0.end());
+  std::sort(c1.begin(), c1.end());
+
+  int64_t sum = 0;
+
+  for(int i = 0; i < 1000; i++)
+  {
+    sum += abs(c0[i] - c1[i]);
+  }
+
+  std::cout << sum << std::endl;
+
+  // part 2
+  sum = 0;
+
+  for(int i = 0; i < 1000; i++)
+  {
+    sum += (c0[i] * r_counts[c0[i]]); 
+  }
+
+  std::cout << sum << std::endl;
+}
